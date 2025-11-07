e384bb70c2c79302a6b6daa9780cc75ecba8a1d9
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-07 22:14:14 +0000

    loaded new gem and demonstrated solution

---
+++ apiary/config/initializers/cors.rb
@@ -0,0 +1,11 @@
+# CORS for the API
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://localhost'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
