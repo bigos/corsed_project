11a4b016f683d2ddbeb7ae9db79b79d5f2d8416f
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-07 23:20:56 +0000

    interesting discovery

--- apiary/config/initializers/cors.rb
+++ apiary/config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost'
+    origins 'https://localhost', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
