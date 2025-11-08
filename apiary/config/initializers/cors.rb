7cf78c49ba1ae61130655c8c9733866c8d1a6147
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-08 00:54:13 +0000

    irrelevant links

--- apiary/config/initializers/cors.rb
+++ apiary/config/initializers/cors.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://localhost',      'http://localhost:3000',
-            'https://localhost:5010', 'http://localhost:3001'
+            'https://localhost:5010/secondary', 'http://localhost:3001'
 
     resource '*',
              headers: :any,
