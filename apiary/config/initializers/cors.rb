3ae08363fccbf1555956da3b710654bf0a76acb7
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-08 00:01:13 +0000

    cleanup of origins

--- apiary/config/initializers/cors.rb
+++ apiary/config/initializers/cors.rb
@@ -2,11 +2,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*', 'https://localhost', 'http://localhost:3000',
-            'https://localhost:5010/secondary', 'http://localhost:3001'
+    origins 'https://localhost',      'http://localhost:3000',
+            'https://localhost:5010', 'http://localhost:3001'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
